2b96dd121a8beac759221f2c7c66137f0f1ade1e
Louis Basile <louiebasile@gmail.com>
2026-06-09 17:51:29 -0500

    Add animation to search empty view

--- Boltstorm/ContentView.swift
+++ Boltstorm/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     @State private var service = ScryfallService()
     @State private var query = ""
+    @State private var shouldAnimateMagnifier = true
+    @State private var shouldAnimateContentUnavailableView = true
 
     var body: some View {
         NavigationStack {
@@ -22,11 +24,32 @@ struct ContentView: View {
                 } else if let error = service.errorMessage {
                     ContentUnavailableView(error, systemImage: "magnifyingglass")
                 } else if service.results.isEmpty {
-                    ContentUnavailableView(
-                        "Search for Cards",
-                        systemImage: "rectangle.stack.badge.magnifyingglass",
-                        description: Text("Enter a card name to find matching cards.")
-                    )
+                    ContentUnavailableView {
+                        Label {
+                            Text("Search for Cards")
+                        } icon: {
+                            Image(systemName: "text.magnifyingglass")
+                                .symbolRenderingMode(.palette)
+                                .foregroundStyle(.black, .blue)
+                                .symbolEffect(.drawOn, options: .speed(0.1),  isActive: shouldAnimateMagnifier)
+                                .task {
+                                    do {
+                                        try await Task.sleep(for: .seconds(1.8))
+                                        shouldAnimateMagnifier = false
+                                    } catch {
+                                    }
+                                }
+                        }
+                    }
+                    description: {
+                        Text("Enter a card name to find matching cards.")
+                    }
+                    .opacity(shouldAnimateContentUnavailableView ? 0 : 1)
+                    .onAppear {
+                        withAnimation(.easeIn(duration: 0.3).delay(0.8)) {
+                            shouldAnimateContentUnavailableView = false
+                        }
+                    }
                 }
             }
             .navigationTitle("Boltstorm")
